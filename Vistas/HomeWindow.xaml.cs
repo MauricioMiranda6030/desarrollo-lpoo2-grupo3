01f66009df02c95c7d2ac6612a806888023d73a9
NikoVelazco <129793948+NikoVelazco@users.noreply.github.com>
2024-09-14 02:33:26 -0300

    Subo Front y Back login

--- Vistas/HomeWindow.xaml.cs
+++ Vistas/HomeWindow.xaml.cs
@@ -9,17 +9,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Vistas
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for HomeWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class HomeWindow : Window
     {
-        public MainWindow()
+        public HomeWindow()
         {
             InitializeComponent();
         }
