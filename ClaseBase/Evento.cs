ded8375ff8dc313c688c60357319c0dd18a5b6f9
MartinSosa7 <martinchoelias7@gmail.com>
2024-09-14 02:56:24 -0300

    Clases Disciplina, Evento y Competencia

---
+++ ClaseBase/Evento.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClaseBase
+{
+    class Evento
+    {
+        public int Id { get; set; }
+        public int CompetitionId { get; set; }
+        public int AthleteId { get; set; }
+        public String State { get; set; }
+        public DateTime InitialDate { get; set; }
+        public DateTime FinalDate { get; set; }
+    }
+
+}
