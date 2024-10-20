de3049e438eb1cb366d7a05505d4eae57821bd65
NikoVelazco <129793948+NikoVelazco@users.noreply.github.com>
2024-10-20 20:00:09 -0300

    Config

--- Vistas/obj/x86/Debug/Vistas_Content.g.i.cs
+++ Vistas/obj/x86/Debug/Vistas_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("estadoscomp.xml")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/messi.jpeg")]
 
 
