fa63c33f176c5b883ef680269b0d9c9d566dbb73
NikoVelazco <129793948+NikoVelazco@users.noreply.github.com>
2024-10-22 01:07:29 -0300

    Inge crack

--- ClaseBase/Atleta.cs
+++ ClaseBase/Atleta.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ComponentModel;
 
 namespace ClaseBase
 {
-    public class Atleta
+    public class Atleta : IDataErrorInfo
     {
         public int Id { get; set; }
         public String DNI { get; set; }
@@ -21,5 +22,38 @@ namespace ClaseBase
         public String Email { get; set; }
 
         public Atleta() { }
+
+        public string Error
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                string result = null;
+                switch (columnName)
+                {
+                    case "DNI":
+                        if (string.IsNullOrEmpty(DNI))
+                            result = "El DNI es obligatorio.";
+                        break;
+                    case "Apellido":
+                        if (string.IsNullOrEmpty(Apellido))
+                            result = "El Apellido es obligatorio.";
+                        break;
+                    case "Altura":
+                        if (Altura <= 0)
+                            result = "La altura debe ser mayor a cero.";
+                        break;
+                    case "Peso":
+                        if (Peso <= 0)
+                            result = "El peso debe ser mayor a cero.";
+                        break;
+                }
+                return result;
+            }
+        }
     }
 }
